334886bc937a01a57fff6cf4032ad5f73ea869ab
A_A_ron <spaaron@umich.edu>
2019-02-25 00:23:57 -0500

    added jinja templates and a basic layout page

--- mysite/sites/views.py
+++ mysite/sites/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-	return HttpResponse('HELLO')
+	#return HttpResponse('HELLO')
+	return render(request, 'sites/index.html', {
+		'title': 'Help grow the web with referrals!!'
+		})
