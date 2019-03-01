6e1b59983ba0c8dd54c8bc96a6cd04641ac774da
A_A_ron <spaaron@umich.edu>
2019-02-28 21:35:34 -0500

    tweaked Procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,5 +129,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #configure Django for Heroku
-import django_heroku
 django_heroku.settings(locals())
